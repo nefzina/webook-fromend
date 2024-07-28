e5faff1b2f86dc09d785dab9c2fe92bd061e661e
Alexandre GOSSARD <hope@Host-001.lan>
2024-07-28 23:47:11 +0200

    push map

--- src/app/book/domain/models/book.ts
+++ src/app/book/domain/models/book.ts
@@ -11,10 +11,12 @@ export class Book{
   isbn: string;
   review: string;
   bookCategory: ICategory;
+  latitude: number;
+  longitude: number;
 
   constructor(book_id: number, book_name: string, book_image: IMedia, book_author: string,
               book_edition: string, book_resume: string, book_isbn: string, book_review: string,
-              book_category:ICategory) {
+              book_category:ICategory, book_latitude: number, book_longitude: number) {
     this.id = book_id;
     this.name = book_name;
     this.coverImage = book_image;
@@ -24,6 +26,8 @@ export class Book{
     this.isbn = book_isbn;
     this.review = book_review;
     this.bookCategory=book_category;
+    this.latitude = book_latitude;
+    this.longitude = book_longitude;
   }
 
 
